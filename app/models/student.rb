c4709f0f6fb69610e9fee202cdb4bf06e42bd60d
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-06-14 22:05:12 -0400

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
-
+ attr_reader :first_name
+  @@all=[]
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  
+  def boatingtests
+    BoatingTest.all.select{|boatingtest|boatingtest.student == self}
+  end
+  def all_instructors
+    boatingtests.map{|boat| boat.instructor}
+  end
+  
+  def self.find_student(first_name)
+  Student.all.find{|student| student.first_name == first_name}
+   end
+ 
+ def passed
+   boatingtests.select{|boat| boat.status == "passed"}
+ end
+   
+  def grade_percentage
+    @grade= (passed.count.to_f/boatingtests.count)* 100
+   @grade
+  end
+  def add_boating_test(boatingtest,status, instructor)
+    BoatingTest.new(self,boatingtest,status, instructor)
+  end
 end
