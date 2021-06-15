5a42dfe8c4a52b789db95f7fca11a31edb98fe96
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-06-14 22:04:31 -0400

    Update boatingtest.rb

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,14 @@
 class BoatingTest
-
+ attr_reader :student, :boatingtest, :status,:instructor
+  @@all=[]
+  def initialize(student, boatingtest, status, instructor)
+    @student = student
+    @boatingtest = boatingtest
+    @instructor = instructor
+    @status = status
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
